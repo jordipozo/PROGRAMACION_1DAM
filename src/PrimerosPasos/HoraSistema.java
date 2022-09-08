ff22c09dd0bc981af4367972142b71212234e806
Jordi Pozo <jpozoc@hotmail.com>
2022-09-08 23:43:54 +0200

    Actualización ficheros ejemplo

---
+++ src/PrimerosPasos/HoraSistema.java
@@ -0,0 +1,22 @@
+package PrimerosPasos;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+import java.time.LocalTime;
+/**
+ *
+ * @author jordi
+ */
+public class HoraSistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        System.out.println("Hola.");
+        System.out.println("La hora del sistema es: ");
+        System.out.println(LocalTime.now());
+    }
+}
